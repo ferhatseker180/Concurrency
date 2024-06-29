e774ab78c2e1cfe6261d14a036f4dfdd35d2c36c
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-06-29 17:44:22 +0300

    Thread konusuna başlangıç yapıldı, Custom Thread oluşturuldu ve hem senkron hem de asenkron şekilde çalıştırıldı. Runnable arayüzü uygulandı.

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -36,6 +36,21 @@ public class Main {
         // Start ile asenkron çalışması için çağrılır ve eş zamanlı olarak çalışır.
         // Run ile çağırırsak senkron çalışır ve diğer işlemin bitmesini bekleyip çalışmaya başlar.
 
+        // Runnable :
+        Runnable runnable = () ->{
+            for (int i = 0; i <= 8; i++) {
+                System.out.print(" 2 ");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(250);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread myThread = new Thread(runnable);
+        myThread.start();
+
         for (int i = 0 ; i <=3; i++){
             System.out.print(" 0 ");
             try {
